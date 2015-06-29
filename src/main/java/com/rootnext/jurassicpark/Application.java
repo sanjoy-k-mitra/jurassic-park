bfdf6ce34ca9ec713e7c0df924a8a345f876b1d6
Sanjoy Mitra <sanjoy120@gmail.com>
2015-06-29 20:05:16 +0600

    Application configuration

--- src/main/java/com/rootnext/jurassicpark/Application.java
+++ src/main/java/com/rootnext/jurassicpark/Application.java
@@ -1,14 +1,27 @@
 package com.rootnext.jurassicpark;
 
+import com.rootnext.jurassicpark.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Created by sanjoy on 6/29/15.
  */
 @SpringBootApplication
-public class Application {
+public class Application{
+    @Autowired
+    CageRepository cageRepository;
+    @Autowired
+    DinosaurRepository dinosaurRepository;
+    @Autowired
+    SpeciesRepository speciesRepository;
+
     public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class);
     }
+
+
 }
