b8b4d9e6340789adcca2f15c81204ceef80389c5
Sanjoy Mitra <sanjoy120@gmail.com>
2015-06-29 15:14:55 +0600

    Init Framework and project setup

---
+++ src/main/java/com/rootnext/jurassicpark/Application.java
@@ -0,0 +1,14 @@
+package com.rootnext.jurassicpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sanjoy on 6/29/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
